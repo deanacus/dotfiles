a51edd49fb6d1ea7c0541041846fe954305ad4d9
Dean Harris <dean@harris.tc>
2013-01-24 20:27:06 +1000

    Add bash_exports to setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bash_aliases bash_functions bash_profile bsh_prompt"    # list of files/folders to symlink in homedir
+files="bash_aliases bash_functions bash_profile bsh_prompt bash_exports"    # list of files/folders to symlink in homedir
 
 ##########
 
