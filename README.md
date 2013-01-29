a7e552dd2dd97239343da4b6c1058482c9a9bfd1
Dean Harris <dean@harris.tc>
2013-01-29 17:21:50 +1000

    Update installation process

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Dotfiles
 
-Dem files that starts with the dots.
+Just another dotfiles repo.
+
+I don't spend a whole lot of time on the command line but when I do I want it to work for me, not the other way around.
 
 ##Installation: 
+
 	git clone git://github.com/deanacus/dotfiles ~/dotfiles
 	cd ~/dotfiles
-	./setup.sh
+	./dotfiles.sh
---
+++ dotfiles.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#
+# dotfiles.sh
+# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
+# Shamelessy ripped from https://github.com/michaeljsmalley/dotfiles
+# 
+
+# Variables 
+
+# Dotfiles directory
+srcdir=~/dotfiles
+
+# Backup directory
+bkdir=~/dotfiles_old
+
+# Target directory
+tgdir=~
+
+# Dotfiles to be linked
+files=$(cat sourcefiles.txt)
+
+
+# Functions
+
+function Usage() {
+echo "Usage: ./dotfiles.sh <cmd>
+
+ Commands:
+	push			Push dotfiles from source to ~
+	pull			Pull dotfiles from ~ to source
+	clone <path>	Create a clone of your source directory at <path>
+"
+}
+
+function Push () {
+	for file in $files; do
+		if [ -f $tgdir/.$file ]; then
+			if [ -d $bkdir ]; then
+				echo "Moving $tgdir/.$file to $bkdir/"
+				mv $tgdir/.$file $bkdir/$file
+			else
+				mkdir -p $bkdir
+				echo "Moving $tgdir/.$file to $bkdir/"
+				mv $tgdir/.$file $bkdir/$file
+			fi
+		fi
+		echo "Copying $file to $tgdir/$file."
+		cp $srcdir/$file $tgdir/.$file
+	done
+}
+
+
+function Pull () {
+	for file in $files; do
+		if [ -d $bkdir ]; then
+			if [ -f $srcdir/$file ]; then
+				echo "Moving $source/$file to $bkdir"
+				mv $srcdir/$file $bkdir/$file
+			fi
+		else
+			mkdir -p $bkdir
+			if [ -f $srcdir/$file ]; then
+				echo "Moving $source/$file to $bkdir"
+				mv $srcdir/$file $bkdir/$file
+			fi
+		fi
+		echo "Copying $tgdir/.$file to $srcdir."
+		cp $tgdir/$file $srcdir/$file
+	done
+}
+
+function Clone () {
+	if [ -z $2 ]; then
+		echo "No target directory supplied"
+		return 1
+		exit
+	else
+		for file in $files; do
+			mkdir -p $2
+			cp $srcdir/$file $2/$file
+		done
+	fi
+}
+
+case $1 in
+    push)	Push	;;
+    pull)	Pull	;;
+	clone)	Clone	;;
+    *)		Usage	;;
+esac
