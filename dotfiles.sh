0bc9b4cf3046b785e38ab66a562be463318bfcfb
Dean Harris <dean@harris.tc>
2013-01-29 22:56:05 +1000

    Added appropriate shell header

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 #
-# dotfiles.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-# Shamelessy ripped from https://github.com/michaeljsmalley/dotfiles
-# 
+# AUTHOR: Dean Harris
+# DATE: 29/1/2013
+# VERSION: 0.7
+#
+# DESCRIPTION:
+# Script for managing dotfiles. Takes a copy of the dotfiles from ~/
+# and moves them to a backup directory then copies source files to ~/
+# Ignores the de facto standard behaviour of creating symlinks in
+# favour of creating actual copies of the source files. Intended for
+# use with my dotfiles repo https://github.com/deanacus/dotfiles.
+#
+# Started life as a rip of 
+# https://github.com/michaeljsmalley/dotfiles then morphed into its
+# current state based on https://github.com/insanum/nostalgic
+#
 
-# Variables 
+# VARIABLES
 
 # Dotfiles directory
 srcdir=~/dotfiles
@@ -16,7 +27,7 @@ bkdir=~/dotfiles_old
 # Target directory
 tgdir=~
 
-# Dotfiles to be copied
+# Dotfiles to be copied (sourced from textfile)
 files=$(cat $srcdir/sourcefiles.txt)
 
 
@@ -32,43 +43,41 @@ echo "Usage: ./dotfiles.sh <cmd>
 "
 }
 
+# Push function:
+# Backs up current working files from ~/ to backup directory then copies source files to ~/
 function Push () {
 	for file in $files; do
 		if [ -f $tgdir/.$file ]; then
-			if [ -d $bkdir ]; then
-				echo "Moving $tgdir/.$file to $bkdir/"
-				mv $tgdir/.$file $bkdir/$file
-			else
+			if [ ! -d $bkdir ]; then
 				mkdir -p $bkdir
-				echo "Moving $tgdir/.$file to $bkdir/"
-				mv $tgdir/.$file $bkdir/$file
-			fi
+			fi	
+			echo "Moving $tgdir/.$file to $bkdir/"
+			mv $tgdir/.$file $bkdir/$file
 		fi
 		echo "Copying $file to $tgdir/$file."
 		cp $srcdir/$file $tgdir/.$file
 	done
 }
 
+# Pull function:
+# Moves current source files to a backup directory, then copies working files from ~/ to ensure that source files match working files
 
 function Pull () {
 	for file in $files; do
-		if [ -d $bkdir ]; then
-			if [ -f $srcdir/$file ]; then
-				echo "Moving $source/$file to $bkdir"
-				mv $srcdir/$file $bkdir/$file
+		if [ -f $srcdir/$file ]; then
+			if [ ! -d $bkdir ]; then
+				mkdir -p $bkdir
 			fi
-		else
-			mkdir -p $bkdir
-			if [ -f $srcdir/$file ]; then
-				echo "Moving $source/$file to $bkdir"
-				mv $srcdir/$file $bkdir/$file
-			fi
+			echo "Moving $source/$file to $bkdir"
+			mv $srcdir/$file $bkdir/$file
 		fi
 		echo "Copying $tgdir/.$file to $srcdir."
-		cp $tgdir/$file $srcdir/$file
+		cp $tgdir/.$file $srcdir/$file
 	done
 }
 
+# Clone function:
+# Creates a fresh version of the listed files in a clean, non-git directory for you to start playing with.
 function Clone () {
 	if [ -z $1 ]; then
 		echo "No target directory supplied"
