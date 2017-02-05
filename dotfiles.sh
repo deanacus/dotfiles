7b8dccf1474d4e2b1841fc1f566fbf8311a79a21
Dean Harris <deanacus@Deans-MacBook-Pro.local>
2017-02-05 22:41:52 +1000

    Update bash prompt, and correct installation shell script

--- dotfiles.sh
+++ dotfiles.sh
@@ -9,5 +9,5 @@
 
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
 ln -s ~/dotfiles/inputrc ~/.inputrc
-ln -s ~/dotfiles/gitconfid ~/.gitconfig
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/vimrc ~/.vimrc
