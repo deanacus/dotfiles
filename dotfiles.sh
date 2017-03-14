15a2ddc501c894061089d416b2eec069396879fc
Dean Harris <dean@harris.tc>
2017-03-14 15:50:37 +1000

    Finally got around to fixing the git branch/status in my git prompt

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,6 +8,7 @@
 # Stupid simple dotfile symlink creator.
 
 ln -s ~/dotfiles/bash_profile ~/.bash_profile
+ln -s ~/dotfiles/bash_prompt ~/.bash_prompt
 ln -s ~/dotfiles/inputrc ~/.inputrc
 ln -s ~/dotfiles/gitconfig ~/.gitconfig
 ln -s ~/dotfiles/vimrc ~/.vimrc
