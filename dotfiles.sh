d8ab73ddd0338c3bd869056cf9c851cdc5fbf8ea
Dean Harris <dean@harris.tc>
2017-05-09 12:12:45 +1000

    Removed unneeded files and added atom config

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,9 +7,29 @@
 # DESCRIPTION:
 # Stupid simple dotfile symlink creator.
 
-ln -s ~/dotfiles/bash_profile ~/.bash_profile
-ln -s ~/dotfiles/bash_prompt ~/.bash_prompt
-ln -s ~/dotfiles/inputrc ~/.inputrc
-ln -s ~/dotfiles/gitconfig ~/.gitconfig
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+echo ""
+echo "Linking .bash_profile"
+ln -sf ~/dotfiles/bash_profile ~/.bash_profile
+echo ""
+echo "Linking .inputrc"
+ln -sf ~/dotfiles/inputrc ~/.inputrc
+echo ""
+echo "Linking .gitconfig"
+ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+
+# Atom
+if [ ! -d ~/.atom/ ]; then
+  echo ""
+  echo "Atom not installed"
+else
+  echo ""
+  echo "Installing any Atom packages not already installed"
+  PACKAGES="$(< ./atom/atom-packages)"
+  for PACKAGE in $PACKAGES; do
+    if [ ! -d ~/.atom/packages/$PACKAGE ]; then
+        apm install $PACKAGE
+    fi
+  done
+fi
+echo ""
+echo "Your dotfiles have been installed"
