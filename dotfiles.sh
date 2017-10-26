d01ab6d28a6c6a88f399de77f501ba15540f56cf
Dean Harris <dean_harris@sunsuper.com.au>
2017-10-27 09:29:12 +1000

    Update prompt, add VSCODE settings, update installation script to automate testing for work/personal

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,18 +7,23 @@
 # DESCRIPTION:
 # Stupid simple dotfile symlink creator.
 
+PLATFORM=`uname`
+
 echo ""
 echo "Linking .bash_profile"
 ln -sf ~/dotfiles/bash_profile ~/.bash_profile
 echo "Linking .inputrc"
 ln -sf ~/dotfiles/inputrc ~/.inputrc
-echo "Are you at work?"
-read enviro
-echo "Linking .gitconfig"
-if [ $enviro == "y" ]; then
+if [[ $PLATFORM == 'MSYS_NT-6.1' ]]; then
+  echo "Linking .gitconfig"
   ln -sf ~/dotfiles/gitconfig.work ~/.gitconfig
-else
+  echo "Linking VSCode settings"
+  ln -sf ~/dotfiles/vscode.json $APPDATA/Code/User/settings.json
+elif [[ $PLATFORM == 'Darwin' ]]; then
+  echo "Linking .gitconfig"
   ln -sf ~/dotfiles/gitconfig ~/.gitconfig
+  echo "Linking VSCode settings"
+  ln -sf ~/dotfiles/vscode.json $HOME/Library/Application Support/Code/User/settings.json
 fi
 echo ""
 echo "Your dotfiles have been installed"
