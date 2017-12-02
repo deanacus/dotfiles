419bfb35c2326423193b28e40766cbe73a7416e1
Dean Harris <dean@harris.tc>
2017-12-02 11:33:54 +1000

    Add Hyper configuration, adjust how I deal with .gitconfig and keep VSCode config up to date.

--- dotfiles.sh
+++ dotfiles.sh
@@ -7,23 +7,25 @@
 # DESCRIPTION:
 # Stupid simple dotfile symlink creator.
 
-PLATFORM=`uname`
+PLATFORM=`hostname`
 
 echo ""
 echo "Linking .bash_profile"
 ln -sf ~/dotfiles/bash_profile ~/.bash_profile
 echo "Linking .inputrc"
 ln -sf ~/dotfiles/inputrc ~/.inputrc
-if [[ $PLATFORM == 'MSYS_NT-6.1' ]]; then
-  echo "Linking .gitconfig"
-  ln -sf ~/dotfiles/gitconfig.work ~/.gitconfig
-  echo "Linking VSCode settings"
-  ln -sf ~/dotfiles/vscode.json $APPDATA/Code/User/settings.json
-elif [[ $PLATFORM == 'Darwin' ]]; then
-  echo "Linking .gitconfig"
+echo "Linking .gitconfig"
   ln -sf ~/dotfiles/gitconfig ~/.gitconfig
-  echo "Linking VSCode settings"
-  ln -sf ~/dotfiles/vscode.json $HOME/Library/Application Support/Code/User/settings.json
+if [[ $PLATFORM == 'MSYS_NT-6.1' ]]; then
+  git config --global user.email "dean.harris@comparethemarket.com.au"
+elif [[ $PLATFORM == 'Deans-MacBook-Pro.local' ]]; then
+  git config --global user.email "dean@harris.tc"
 fi
+
+echo "Linking VSCode settings"
+ln -sf ~/dotfiles/vscode.json $HOME/Library/Application\ Support/Code/User/settings.json
+echo "Linking .hyperjs"
+ln -sf ~/dotfiles/hyperjs ~/.hyperjs
+
 echo ""
 echo "Your dotfiles have been installed"
