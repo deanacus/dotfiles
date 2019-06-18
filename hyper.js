45599eed689fa8fcc453e61964394497477fd8b1
Dean Harris <dean.harris@comparethemarket.com.au>
2019-06-18 10:46:57 +1000

    Application config changes and new shell alias for sleep

--- hyper.js
+++ hyper.js
@@ -116,7 +116,8 @@ module.exports = {
 	//   `@company/project`
 	//   `project#1.0.1`
 	plugins: [
-	    'hyper-oceanic-next',
+		'hyper-oceanic-next',
+		'hyper-search',
 	],
 
 	// in development, you can create a directory under
