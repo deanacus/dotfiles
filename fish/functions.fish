345ef784efb2e4afb698ef7099929cf90e511cf2
Dean Harris <dean.harris@comparethemarket.com.au>
2019-12-16 08:06:16 +1000

    feat(function): add new clone function

--- fish/functions.fish
+++ fish/functions.fish
@@ -33,3 +33,9 @@ end
 function fuck -d "When you're really annoyed that something didn't work right"
   eval command sudo $history[1]
 end
+
+function clone -a user repo path -d "Clone a github repository"
+  if test $path && test -d $path && test -z (ls $path)
+    git clone git@github.com:$user/$repo.git $path
+  end
+end
