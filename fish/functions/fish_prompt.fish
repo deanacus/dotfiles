2e5427d8392f7df36590eeb1e1e0c1a6699d6899
Dean Harris <dean@repeat.gg>
2020-03-31 15:25:54 +1000

    update prompt symbols

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,21 +3,23 @@ set -g fish_prompt_pwd_dir_length 0
 # Git prompt
 set -g __fish_git_prompt_showdirtystate 'yes'
 set -g __fish_git_prompt_showupstream 'yes'
+set -g __fish_git_prompt_showstashstate 'yes'
 set -g __fish_git_prompt_color_branch black -o
 set -g __fish_git_prompt_color_dirtystate red
 set -g __fish_git_prompt_color_stagedstate green
+set -g __fish_git_prompt_color_stashstate cyan
 set -g __fish_git_prompt_color_upstream cyan
 
 # Git Characters
 set -g __fish_git_prompt_char_stateseparator ''
-set -g __fish_git_prompt_char_stashedstate '*'
+set -g __fish_git_prompt_char_stashstate '⭠'
 set -g __fish_git_prompt_char_dirtystate '*'
-set -g __fish_git_prompt_char_stagedstate '*'
+set -g __fish_git_prompt_char_stagedstate '⭢'
 set -g __fish_git_prompt_char_upstream_prefix ''
 set -g __fish_git_prompt_char_upstream_equal ''
-set -g __fish_git_prompt_char_upstream_ahead '⇡'
-set -g __fish_git_prompt_char_upstream_behind '⇣'
-set -g __fish_git_prompt_char_upstream_diverged '⇡⇣'
+set -g __fish_git_prompt_char_upstream_ahead '⭡'
+set -g __fish_git_prompt_char_upstream_behind '⭣'
+set -g __fish_git_prompt_char_upstream_diverged '⮁'
 
 function _print_in_color
   set -l string $argv[1]
