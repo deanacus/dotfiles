fcf6add4003f799234e205746af24218eacb390d
Dean Harris <dean@repeat.gg>
2020-04-08 12:02:43 +1000

    feat: remove unnecessary variables

--- fish/variables.fish
+++ fish/variables.fish
@@ -1,31 +1,9 @@
 # This file contains fish universal variable definitions.
 # VERSION  3.0
 set -U --export EDITOR code
-set -U fish_color_autosuggestion 555\x1ebrblack
-set -U fish_color_cancel \x2dr
-set -U fish_color_command normal
-set -U fish_color_comment 990000
-set -U fish_color_cwd green
-set -U fish_color_cwd_root red
-set -U fish_color_end 009900
-set -U fish_color_error white
-set -U fish_color_escape 00a6b2
-set -U fish_color_history_current \x2d\x2dbold
-set -U fish_color_host normal
-set -U fish_color_match \x2d\x2dbackground\x3dbrblue
-set -U fish_color_normal normal
-set -U fish_color_operator 00a6b2
-set -U fish_color_param 00afff
-set -U fish_color_quote 999900
-set -U fish_color_redirection 00afff
-set -U fish_color_search_match bryellow\x1e\x2d\x2dbackground\x3dbrblack
-set -U fish_color_selection white\x1e\x2d\x2dbold\x1e\x2d\x2dbackground\x3dbrblack
-set -U fish_color_user brgreen
-set -U fish_color_valid_path \x2d\x2d
-set -U fish_greeting ""
-set -U fish_key_bindings fish_default_key_bindings
-set -U fish_pager_color_description B3A06D\x1eyellow
-set -U fish_pager_color_prefix white\x1e\x2d\x2dbold\x1e\x2d\x2dunderline
-set -U fish_pager_color_progress brwhite\x1e\x2d\x2dbackground\x3dcyan
 set -g fish_function_path ~/dotfiles/fish/functions $fish_function_path
 set -g fish_user_paths ~/dotfiles/bin ~/dotfiles/bin/local $fish_user_paths
+set -U --export PLATFORM (eval os)
+set -U fish_greeting ""
+set -U fish_color_valid_path blue
+set -U fish_color_error normal
