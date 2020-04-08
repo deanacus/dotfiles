33a7834a98c1c1b69e8d0c859c1ab5297dce5f79
Dean Harris <dean@repeat.gg>
2020-04-08 12:02:30 +1000

    feat: add cross platform open command

---
+++ fish/functions/open.fish
@@ -0,0 +1,14 @@
+function open -a path -d "Cross platform file manager launcher"
+  switch $PLATFORM
+    case windows
+        explorer.exe $path
+    case darwin
+        open $path
+    case linux
+        echo I dunno what to do with this
+    case ''
+        echo "No platform set"
+  end
+end
+
+
