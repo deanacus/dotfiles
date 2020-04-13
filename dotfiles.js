d5ad0ed79684714b07effcd67483f8342d6c4bf1
Dean <dean@harris.tc>
2020-04-13 21:36:48 +1000

    Start laying out a potential js implementation

---
+++ dotfiles.js
@@ -0,0 +1,41 @@
+const os = require('fs');
+
+const installVim = () => {
+  // Link ./vim/vimrc to ~/.vimrc
+  // Link./vim/autoload/plug.vim to ~/vim/autoload/plug.vim
+}
+
+const installGit = () => {
+  // prompt for git user.name
+  // promt for git user.email
+  // write to ~/.gitconfig.local
+  // link ./git/gitconfig ~/.gitconfig
+};
+
+const installFish = () => {
+  // Link ./fish/config.fish to ~/.config/fish/config.fish
+};
+
+const installNode = () => {
+  // Link ./npmrc to ~/.npmrc
+}
+
+const installVSC = () => {
+  // Link ./vscode/settings.json to ~/Library/Application\ Support/Code/User/settings.json
+  // Link ./vscode/settings.json to ~/.vscode-server/data/Machine/settings.json
+  // Link ./vscode/keybindings.mac.json to ~/Library/Application\ Support/Code/User/keybindings.json
+  // Link ./vscode/keybindings.win.json to ~/.vscode-server/data/Machine/keybindings.json
+  // Link ./vscode/snippets/* to ~/Library/Application\ Support/Code/User/snippets/*
+  // Link ./vscode/snippets/* to ~/.vscode-server/data/Machine/snippets/*
+}
+
+const installTerm = () => {
+  // Link ./hyper.js to ~/.hyper.js
+  // Link ./alacritty.yml to ~/.alacritty.yml
+};
+
+const installWindowManager = () => {
+  // Mac
+  //
+  // Link ./spectacle.json to ~/Library/Application\ Support/Spectacle/Shortcuts.json
+}
