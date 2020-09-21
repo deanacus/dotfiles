938c8332f9fa553efd00ab395c97a5ab46ea76bc
Dean Harris <dean@harris.tc>
2020-09-21 15:52:21 +1000

    Update shell variables

--- fish/variables.fish
+++ fish/variables.fish
@@ -1,11 +1,6 @@
 # This file contains fish universal variable definitions.
 # VERSION  3.0
-set -U --export EDITOR nvim
+set -g EDITOR nvim
 set -g fish_function_path ~/dotfiles/fish/functions $fish_function_path
 set -g fish_user_paths ~/dotfiles/bin ~/dotfiles/bin/local /usr/local/opt/fzf/bin /usr/local/sbin $fish_user_paths
-set -U --export PLATFORM (eval os)
-set -U fish_greeting ""
-set -U fish_color_valid_path blue
-set -U fish_color_error normal
 set -U FZF_DEFAULT_COMMAND 'fd --type f'
-set -U --export DISPLAY localhost:0.0 # Set up the display for WSL x11
