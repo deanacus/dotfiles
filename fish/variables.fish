0bcaab87f3a2b511923f89bac0753623f9c12089
Dean Harris <dean@harris.tc>
2021-01-19 16:25:58 +1000

    chore(tidyup): break fish user_paths up into multiple lines so I can read it

--- fish/variables.fish
+++ fish/variables.fish
@@ -1,8 +1,22 @@
 # This file contains fish universal variable definitions.
 # VERSION  3.0
+
+# Make neovim my editor
 set -g EDITOR nvim
+
+# Add my own function path
 set -g fish_function_path ~/dotfiles/fish/functions $fish_function_path
-set -g fish_user_paths ~/bin ~/dotfiles/bin ~/dotfiles/bin/local ~/.cargo/bin /usr/local/opt/fzf/bin /usr/local/sbin /usr/local/go/bin $fish_user_paths
+
+# Add my own "bin" folder to $PATH
+set -g fish_user_paths ~/bin $fish_user_paths
+# Add another person "bin" folder to $PATH, this time from ~/dotfiles
+set -g fish_user_paths ~/dotfiles/bin $fish_user_paths
+# Add go to $PATH
+set -g fish_user_paths /usr/local/go/bin $fish_user_paths
+# Add globally installed composer packages to $PATH
+set -g fish_user_paths ~/.composer/vendor/bin $fish_user_paths
+
+
 set -U FZF_DEFAULT_COMMAND 'fd --type f'
 
 set fish_greeting ""
