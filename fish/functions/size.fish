a8db80c06032643f089bd1795419af5b4933e3e0
Dean Harris <dean@harris.tc>
2021-01-25 22:19:31 +1000

    moar git extras changes

--- fish/functions/size.fish
+++ fish/functions/size.fish
@@ -2,6 +2,6 @@ function size -a path -d "Print the human readable size of the current directory
   if test $path
     du -sh $path | awk '{print $1}'
   else
-    du -sh $path | awk '{print $1}'
+    du -sh ./ | awk '{print $1}'
   end
 end
