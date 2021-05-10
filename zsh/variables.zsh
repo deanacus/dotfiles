42b37e02634f121f95b6c7a40573e149e3dc6ce3
Dean Harris <dean@harris.tc>
2021-05-10 17:48:33 +1000

    🤷‍♂️

--- zsh/variables.zsh
+++ zsh/variables.zsh
@@ -36,3 +36,8 @@ export LC_ALL='en_AU.UTF-8';
 
 export PATH=$HOME/.npm/bin:$PATH;
 export PATH=$HOME/dotfiles/git/extras:$PATH;
+export PATH=/usr/local/bin:$PATH;
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
